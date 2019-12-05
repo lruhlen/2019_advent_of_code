da2e863e3bf11b84bed1ff350cbe805c020d5a6c
Laurel Ruhlen <laurel.ruhlen@getbraintree.com>
2019-12-05 17:19:49 -0600

    AAARG

--- day3/day3.py
+++ day3/day3.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.sparse import lil_matrix # For EFFICIENCY!!!!!
 
 # Part 1
 default_origin = np.array([0, 0])
@@ -8,14 +9,14 @@ class Grid():
     def __init__(self, nrows=10, ncols=10, origin=default_origin):
         self.nrows = nrows
         self.ncols = ncols
-        self.grid = np.zeros((self.nrows, self.ncols))
+        self.grid = lil_matrix((self.nrows, self.ncols), dtype=np.int8)
         self.origin = origin
         self._current_x = self.origin[0]
         self._current_y = self.origin[1]
 
     def show(self):
         """Display origin point in lower left"""
-        print(self.grid[::-1])
+        print(self.grid.toarray()[::-1])
 
     def parse_instruction(self, inst):
         direction = inst[0]
@@ -31,8 +32,11 @@ class Grid():
 
         return (magnitude, direction)
 
-    def update(self, magnitude, direction):
+    def update(self, instruction):
         """Numpy indexing order: row (y), col (x)"""
+        import pdb; pdb.set_trace()
+
+        magnitude, direction = self.parse_instruction(instruction)
         if direction == "v":
             new_y = self._current_y + magnitude
             
@@ -59,34 +63,12 @@ class Grid():
 
     def build_path(self, instructions):
         for inst in instructions:
-            m, d = self.parse_instruction(inst)
-            self.update(m, d)
-
-
-def test_init():
-    pass
-
-
-def test_update():
-    g = Grid(rows=3, cols=3)
-    g.update(2, 'h')
-    g.update(2, 'v')
-    g.update(-2, 'h')
-    expected_result = np.array([[1, 1, 1], [0, 0, 1], [1, 1, 1]])
-    assert (g.grid == expected_result).all()
-
-
-def test_parse_instructions():
-    my_grid = Grid()
-    assert (my_grid.parse_instruction('U10') == ('col', 10))
-    assert (my_grid.parse_instruction('D10') == ('col', -10))
-    assert (my_grid.parse_instruction('R10') == ('row', 10))
-    assert (my_grid.parse_instruction('L10') == ('row', -10))
-
+            #m, d = self.parse_instruction(inst)
+            self.update(inst)
 
 
 def find_intersections(grid1, grid2):
-    crosses = np.logical_and(grid1, grid2).nonzero()
+    crosses = np.logical_and(grid1.grid.toarray(), grid2.grid.toarray()).nonzero()
     result = np.array(list(zip(crosses[0], crosses[1])))
 
     return result
@@ -106,6 +88,48 @@ def find_closest_intersection(grid1, grid2, origin=default_origin):
         raise Exception("No intersections detected")
 
     return min(distances)
+
+
+def main(l1, l2):
+    l1 = l1.split(',')
+    l2 = l2.split(',')
+
+    g1 = Grid(nrows=1000, ncols=1000, origin=[500,500])
+    g2 = Grid(nrows=1000, ncols=1000, origin=[500,500])
+
+    g1.build_path(l1)
+    g2.build_path(l2)
+
+    result = find_closest_intersection(g1, g2)
+    return result
+    
+
+
+
+# Tests
+
+def test_main():
+    i1 = 'R75,D30,R83,U83,L12,D49,R71,U7,L72'
+    i2 = 'U62,R66,U55,R34,D71,R55,D58,R83'
+    result = main(i1, i2)
+    assert (result == 159)
+
+
+def test_update():
+    g = Grid(nrows=3, ncols=3)
+    g.update(2, 'h')
+    g.update(2, 'v')
+    g.update(-2, 'h')
+    expected_result = np.array([[1, 1, 1], [0, 0, 1], [1, 1, 1]])
+    assert (g.grid == expected_result).all()
+
+
+def test_parse_instructions():
+    my_grid = Grid()
+    assert (my_grid.parse_instruction('U10') == (10, 'v'))
+    assert (my_grid.parse_instruction('D10') == (-10, 'v'))
+    assert (my_grid.parse_instruction('R10') == (10, 'h'))
+    assert (my_grid.parse_instruction('L10') == (-10, 'h'))
 
 
 def test_find_intersections():
